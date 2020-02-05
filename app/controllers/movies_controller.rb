d215d892095bd9399de54c8cbdb3f4d6dde09a6e
edgargarcia01 <edgarm.garcia01@gmail.com>
2020-02-05 23:35:51 +0000

    part 3 stable initial version

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,6 +14,7 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.all_ratings
     @movies = Movie.all
     @sorted = params[:sorted]
+    @filtered_by_ratings = nil
     
     if params[:ratings]
       @filtered_by_ratings = params[:ratings].keys
