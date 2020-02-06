feaa7e4ebfcf0d25c9516478b5ab5050432df12d
edgargarcia01 <edgarm.garcia01@gmail.com>
2020-02-06 23:57:47 +0000

    part 3 added elsif session

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -35,9 +35,9 @@ class MoviesController < ApplicationController
     if params.has_key?(:ratings)
       @filtered_by_ratings = params[:ratings].keys
       session[:ratings] = params[:ratings].keys
-    #elsif session[:ratings]
-      #@filtered_by_ratings = session[:ratings]
-      #sessionState = true
+    elsif session[:ratings]
+      @filtered_by_ratings = session[:ratings]
+      sessionState = true
     #elsif params[:ratings].nil?
       #@filtered_by_ratings = nil
       #session[:ratings] = nil
